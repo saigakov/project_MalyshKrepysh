304b1ce23ed7056a898b79bcc9ee331a56309c68
Konstantin <saigakov@yandex.ru>
2020-05-28 17:59:57 +0300

    add send massage and validation form and mask phone

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -13,13 +13,13 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.yandex.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'saigakov@yandex.ru';                 // Наш логин
-$mail->Password = '833Mi8Bort739';                           // Наш пароль от ящика
+$mail->Username = 'saigakovwork@yandex.ru';                 // Наш логин
+$mail->Password = 'dynnuw-4jagtE-jacver';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('saigakov@yandex.ru', 'Pulse');   // От кого письмо 
-$mail->addAddress('saigakovka@gmail.com');     // Add a recipient
+$mail->setFrom('saigakovwork@yandex.ru', 'Pulse');   // От кого письмо 
+$mail->addAddress('saigakov@yandex.ru');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
