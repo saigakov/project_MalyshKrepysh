80fffec3d228a4169b5aaf50e3b9b1fd2eebf7f0
Konstantin <saigakov@yandex.ru>
2020-06-15 20:28:16 +0300

    finish section catalog

--- src/js/script.js
+++ src/js/script.js
@@ -50,13 +50,24 @@ $(document).ready(function() {
         return false;
     });
 
-    /* $(window).scroll(function() {
-        if ($(this).scrollTop() > 0) {
-            $('.header').fadeOut();
-        } else {
-            $('.header').fadeIn();
-        }
-    }); */
+    //tabs
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog__content').eq(i).toggleClass('catalog__content_active');
+            })
+        });
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
     
 });
 
@@ -80,18 +91,6 @@ window.addEventListener('DOMContentLoaded', () => {
             list.classList.toggle('header__list_active');
         })
     });
-
-    //hide header
-    const header = document.querySelector('header');
-
-    document.onwheel = (event) => {
-        if (event.deltaY > 0 ) {
-            header.classList.add('header_active');
-            header.classList.add('fade');
-        } else {
-            header.classList.remove('header_active');
-        }
-    };
 
     
         
